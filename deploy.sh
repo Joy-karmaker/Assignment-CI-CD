5fa4a3482a8865f3af44d4865718b77c6b50e857
Joy-karmaker <joykarmaker2065@gmail.com>
2025-03-13 20:55:30 +0600

    Added deployment script and workflow

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+APP_NAME="node-app"
+DEPLOY_DIR="/var/www/$APP_NAME"
+GITHUB_WORK_DIR="$HOME/_work/Assignment-CI-CD/Assignment-CI-CD"
+
+echo "Stopping existing application..."
+pm2 stop $APP_NAME || true
+
+echo "Copying new build..."
+rm -rf $DEPLOY_DIR/*
+cp -r $GITHUB_WORK_DIR/* $DEPLOY_DIR/
+
+echo "Installing dependencies..."
+cd $DEPLOY_DIR || exit
+npm install --production
+
+echo "Restarting application..."
+pm2 restart $APP_NAME || pm2 start app.js --name $APP_NAME
+
+echo "Deployment complete!"
